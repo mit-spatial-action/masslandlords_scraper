bdb75a3b7420e69c7c41c3e3dfbac24d6b9a3fc3
Eric Robsky Huntley <ehuntley@mit.edu>
2023-03-13 09:43:39 -0400

    streamline by storing results in single dict

--- scraper.py
+++ scraper.py
@@ -1,86 +1,39 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Mar  3 00:19:53 2023
 
-@author: thomasrael
-"""
-
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 import requests
 import datetime
 
 
+START_DATE = "2020-10-24" 
+page_content = requests.get("https://masslandlords.net/policy/eviction-data/filings-week-ending-" + START_DATE).text.split()
 
-print("––––––––––––––––––––––")
+RESULTS_DICT = {}
 
-# url = list(urllib.request.urlopen("https://masslandlords.net/policy/eviction-data/filings-week-ending-" + date))
-# print (url)
+L = {
+    "W": 0,
+    "E": 0,
+    "C": 0,
+    "SE": 0,
+    "NE": 0,
+    "MS": 0
+}
 
-# for i in range(len(url)):
-#     print (str(url[i]))
-
-W  = []
-E  = []
-C  = []
-SE = []
-NE = []
-MS = []
-
-
-date = "2020-10-24" 
-url = (requests.get("https://masslandlords.net/policy/eviction-data/filings-week-ending-" + date)).text
-urllist = url.split()
-
-
-DIC = {} ##key: date, value: evictions 
-
-
-L = [0,0,0,0,0,0] 
-for i in range(len(urllist)): 
-    if urllist[i] == "metro_south":
-        # print("Metro South:" + urllist[i+1])
-        L[5] = urllist[i+1]
-        MS.append(urllist[i+1])  
-    if urllist[i] == "eastern" and urllist[i+1] != "hampshire":  
-        # print("Eastern:" + urllist[i+1])
-        L[4] = urllist[i+1] 
-        E.append(urllist[i+1]) 
-    if urllist[i] == "western":
-        # print("Western:" + urllist[i+1])
-        L[3] = urllist[i+1]
-        W.append(urllist[i+1]) 
-    if urllist[i] == "southeast":                  
-        # print("Southeast:" + urllist[i+1])
-        L[2] = urllist[i+1]
-        SE.append(urllist[i+1]) 
-    if urllist[i] == "northeast":
-        # print("Northeast:" + urllist[i+1])
-        L[1] = urllist[i+1]
-        NE.append(urllist[i+1]) 
-    if urllist[i] == "central" and urllist [i-1] != "bmc":  
-        # print("Central:" + urllist[i+1])
-        L[0] = urllist[i+1]
-        C.append(urllist[i+1]) 
+for i in range(len(page_content)): 
+    if page_content[i] == "metro_south":
+        L["MS"] = page_content[i+1] 
+    if page_content[i] == "eastern" and page_content[i+1] != "hampshire":
+        L["E"] = page_content[i+1] 
+    if page_content[i] == "western":
+        L["W"] = page_content[i+1]
+    if page_content[i] == "southeast":
+        L["SE"] = page_content[i+1]
+    if page_content[i] == "northeast":
+        L["NE"] = page_content[i+1]
+    if page_content[i] == "central" and page_content [i-1] != "bmc":
+        L["MS"] = page_content[i+1]
     
-    DIC[date] = L
+    RESULTS_DICT[START_DATE] = L
 
-print (DIC)
-
-
-
-
-# "/Users/thomasrael/Desktop/piq/school/2022/UROP/"
-
-
-# workbook = xlsxwriter.Workbook("weekly-filings.xlsx")
-# worksheet = workbook.add_worksheet("FILINGS")
-
-# for i in range(len(DIC.keys())):
-#     worksheet.write(i+1,0,list(DIC.keys())[i])
-#     for j in range(6):
-#         worksheet.write(i+1,j+1,DIC[list(DIC.keys())[i]][j])
-
-# workbook.close()
-
-
+print (RESULTS_DICT)
