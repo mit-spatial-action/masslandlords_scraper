d31776b2c7d46e62699d6dcbf4d7180d53679ee7
Eric Robsky Huntley <ehuntley@mit.edu>
2023-03-13 10:21:08 -0400

    automatically generate weeks list, add 404 test

--- scraper.py
+++ scraper.py
@@ -1,95 +1,64 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Mar  3 00:19:53 2023
 
-@author: thomasrael
-"""
-
-import xlsxwriter
 import requests
 from datetime import datetime
 from datetime import timedelta
+from time import sleep
 
 
+START_DATE = datetime.strptime("2020-10-24", "%Y-%m-%d")
+URL_BASE = "https://masslandlords.net/policy/eviction-data/filings-week-ending-"
+RESULTS_DICT = {} ##key: date, value: evictions
 
-print("––––––––––––––––––––––")
+def create_weeks_list(start_date, end_date = False):
+    """
+    Create a list of week starts between a start and end date.
+    """
+    if not end_date:
+        end_date = datetime.today()
+    date_list = []
+    weekly = start_date
+    while weekly <= end_date:
+        date_list.append(weekly.strftime("%Y-%m-%d"))
+        weekly += timedelta(days=7)
+    return date_list
 
-# url = list(urllib.request.urlopen("https://masslandlords.net/policy/eviction-data/filings-week-ending-" + date))
-# print (url)
+def parse_page(page_request):
+    """
+    Parses requested page.
+    """
+    results = {
+        "W": 0,
+        "E": 0,
+        "C": 0,
+        "SE": 0,
+        "NE": 0,
+        "MS": 0
+    }
+    page = page_request.text.split()
+    for i in range(len(page)): 
+        if page[i] == "metro_south":
+            results["MS"] = page[i+1] 
+        if page[i] == "eastern" and page[i+1] != "hampshire":
+            results["E"] = page[i+1] 
+        if page[i] == "western":
+            results["W"] = page[i+1]
+        if page[i] == "southeast":
+            results["SE"] = page[i+1]
+        if page[i] == "northeast":
+            results["NE"] = page[i+1]
+        if page[i] == "central" and page [i-1] != "bmc":
+            results["MS"] = page[i+1]
+    return results
 
-# for i in range(len(url)):
-#     print (str(url[i]))
-
-W  = []
-E  = []
-C  = []
-SE = []
-NE = []
-MS = []
-
-
-date = "2020-10-24" 
-
-url = (requests.get("https://masslandlords.net/policy/eviction-data/filings-week-ending-" + date)).text
-urllist = url.split()
-
-
-DIC = {} ##key: date, value: evictions 
-j = 0
-while j < 130: ### until when??
+for date in create_weeks_list(START_DATE):
+    request = requests.get(URL_BASE + date)
+    if request.status_code == 404:
+        print(f"No data available for {date}.")
+        continue
+    else:
+        RESULTS_DICT[date] = parse_page(request)
+    sleep(1.5)
     
-    
-    url = (requests.get("https://masslandlords.net/policy/eviction-data/filings-week-ending-" + date)).text
-    urllist = url.split()
-    L = [0,0,0,0,0,0] 
-    for i in range(len(urllist)): 
-        if urllist[i] == "metro_south":
-            # print("Metro South:" + urllist[i+1]) 
-            L[5] = urllist[i+1]
-            MS.append(urllist[i+1])  
-        if urllist[i] == "eastern" and urllist[i+1] != "hampshire":  
-            # print("Eastern:" + urllist[i+1])
-            L[4] = urllist[i+1] 
-            E.append(urllist[i+1]) 
-        if urllist[i] == "western":
-            # print("Western:" + urllist[i+1])
-            L[3] = urllist[i+1]
-            W.append(urllist[i+1]) 
-        if urllist[i] == "southeast":                  
-            # print("Southeast:" + urllist[i+1])
-            L[2] = urllist[i+1]
-            SE.append(urllist[i+1]) 
-        if urllist[i] == "northeast":
-            # print("Northeast:" + urllist[i+1])
-            L[1] = urllist[i+1]
-            NE.append(urllist[i+1]) 
-        if urllist[i] == "central" and urllist [i-1] != "bmc":  
-            # print("Central:" + urllist[i+1])
-            L[0] = urllist[i+1]
-            C.append(urllist[i+1]) 
-        
-        DIC[date] = L
-        
-    date = datetime.strptime(date, "%Y-%m-%d")    
-    date += timedelta(days=7) 
-    date = datetime.strftime(date, "%Y-%m-%d")    
-        
-    j += 1
-print (DIC)
-
-
-"/Users/thomasrael/Desktop/piq/school/2022/UROP/"
-
-
-workbook = xlsxwriter.Workbook("weekly-filings.xlsx")
-worksheet = workbook.add_worksheet("FILINGS")
-
-for i in range(len(DIC.keys())):
-    worksheet.write(i+1,0,list(DIC.keys())[i])
-    for j in range(6):
-        worksheet.write(i+1,j+1,DIC[list(DIC.keys())[i]][j])
-
-workbook.close()
-
 
