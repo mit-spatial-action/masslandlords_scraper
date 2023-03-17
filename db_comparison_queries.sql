bcd93a33341183f956a33df805741cf4de46d588
Eric Robsky Huntley <ehuntley@mit.edu>
2023-03-17 09:49:35 -0400

    update field names for easy spreadsheet work

--- db_comparison_queries.sql
+++ db_comparison_queries.sql
@@ -35,12 +35,12 @@ ORDER BY week_start, week_end;
 SELECT 
 	week_start,
 	week_end,
-	COALESCE(max(n) FILTER (WHERE district = 'central'), 0) AS C_db,
-	COALESCE(max(n) FILTER (WHERE district = 'eastern'), 0) AS E_db,
-	COALESCE(max(n) FILTER (WHERE district = 'metrosouth'), 0) AS MS_db,
-	COALESCE(max(n) FILTER (WHERE district = 'northeast'), 0) AS NE_db,
-	COALESCE(max(n) FILTER (WHERE district = 'southeast'), 0) AS SE_db,
-	COALESCE(max(n) FILTER (WHERE district = 'western'), 0) AS W_db
+	COALESCE(max(n) FILTER (WHERE district = 'central'), 0) AS C_db_noad,
+	COALESCE(max(n) FILTER (WHERE district = 'eastern'), 0) AS E_db_noad,
+	COALESCE(max(n) FILTER (WHERE district = 'metrosouth'), 0) AS MS_db_noad,
+	COALESCE(max(n) FILTER (WHERE district = 'northeast'), 0) AS NE_db_noad,
+	COALESCE(max(n) FILTER (WHERE district = 'southeast'), 0) AS SE_db_noad,
+	COALESCE(max(n) FILTER (WHERE district = 'western'), 0) AS W_db_noad
 FROM (
 	-- This inner query groups filings by week.
 	SELECT 
